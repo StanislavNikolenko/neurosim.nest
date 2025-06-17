ecc44ec4f68fea49ca9165e7ff9dc7e2ef22fc3f
Stanislav <stan.nikolenko89@gmail.com>
2025-06-17 23:02:02 +0300

    chore: add config service usage for envs

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ClientsModule } from '@nestjs/microservices';
 import { Transport } from '@nestjs/microservices';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: `./config/.${process.env.NODE_ENV}.env`,
+    }),
     ClientsModule.register([
       {
         name: 'INGEST_SERVICE',
